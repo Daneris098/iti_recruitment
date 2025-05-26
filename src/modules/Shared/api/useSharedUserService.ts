78c1117b1c118fe7cc59e1fa34694be7daca5471
hersvinlabastida <hersvinlabastida@intellismartinc.com>
2025-05-26 09:50:58 +0800

    Additional Files

--- src/modules/Shared/api/useSharedUserService.ts
+++ src/modules/Shared/api/useSharedUserService.ts
@@ -5,3 +5,7 @@ import { ApplicantResponse, JobOpenings } from "@modules/Shared/types";
 export const useSharedUserService = new BaseService<ApplicantResponse>(apiClient, "/recruitment/applicants");
 
 export const useSharedTransferredPosition = new BaseService<JobOpenings>(apiClient, "recruitment/vacancies");
+
+export const useSharedOrgService = (endpoint?: string) => new BaseService<any>(apiClient, `/recruitment/organization${endpoint}`);
+
+export const useSharedGeneralService = (endpoint?: string) => new BaseService<any>(apiClient, `/general${endpoint}`);
