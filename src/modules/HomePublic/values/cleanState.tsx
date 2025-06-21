cae77de36074409bfde7a7b5de07e9b9f4d9b797
daneris <danerismendoza>
2025-06-21 09:45:50 +0800

    remove initail data clean state

--- src/modules/HomePublic/values/cleanState.tsx
+++ src/modules/HomePublic/values/cleanState.tsx
@@ -322,9 +322,9 @@ export const referenceValue2: Reference = {
 };
 
 export const ApplicationFormVal: ApplicationForm = {
-  generalInformation: GeneralInformationVal2,
-  educationAndEmployment: educationAndEmploymentVal2,
-  familyBackground: familyBackgroundVal2,
-  reference: referenceValue2,
+  generalInformation: GeneralInformationVal,
+  educationAndEmployment: educationAndEmploymentVal,
+  familyBackground: familyBackgroundVal,
+  reference: referenceValue,
   photo: "",
 };
